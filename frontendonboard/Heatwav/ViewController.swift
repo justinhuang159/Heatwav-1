4bbd394cda4cb84132432b68e22633cb3f58bf86
Stanley Zeng <sz1911@nyu.edu>
2018-09-21 19:12:41 -0400

    frontend onboarding first 5 slides

---
+++ frontendonboard/Heatwav/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Heatwav
+//
+//  Created by Stanley Zeng on 9/16/18.
+//  Copyright © 2018 Stanley Zeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
