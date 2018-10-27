747a74979a9ba216af3e869de39fa34321b24659
fireteam99 <raymancheungsy@gmail.com>
2018-10-26 23:53:38 -0400

    updated error messages and finished user routes

--- node/app/routes/user_routes.js
+++ node/app/routes/user_routes.js
@@ -13,9 +13,9 @@ module.exports = function(app, db) {
 			}
 		} catch (err) {
 			if (err.name === 'MongoError') {
-				res.status(409).send(new MyError('There was an issue reading from db: ', [err.message]));
+				res.status(409).send('There was an issue saving to db: ' + err.message);
 			}
-			res.status(500).send(new MyError('Unknown Server Error'));
+			res.status(500).send('Unknown Server Error: ' + err.message);
 		}
 	});
 
@@ -39,7 +39,7 @@ module.exports = function(app, db) {
 			});
 		} catch (err) {
 			if (err.name === 'MongoError') {
-				res.status(409).send(new MyError('There was an issue saving to db: ', [err.message]));
+				res.status(409).send('There was an issue saving to db: ' + err.message);
 			}
 			res.status(500).send(new MyError('Unknown Server Error'));
 		}
@@ -50,7 +50,7 @@ module.exports = function(app, db) {
 		try {
 			const deletedUser = await User.deleteOne({ username: username });
 			if (!deletedUser) {
-				res.status(404).send(new MyError('User not found error: ' + username));
+				res.status(409).send('There was an issue saving to db: ' + err.message);
 			} else {
 				res.status(200).send({
 				response: 'Deleted user: ' + username
@@ -59,16 +59,17 @@ module.exports = function(app, db) {
 			
 		} catch (err) {
 			if (err.name === 'MongoError') {
-				res.status(409).send(new MyError('There was an issue saving to db: ', [err.message]));
+				res.status(409).send('There was an issue saving to db: ' + err.message);
 			}
-			res.status(500).send(new MyError('Unknown Server Error'));
+			res.status(500).send('Unknown Server Error: ' + err.message);
 		}
 	});
 
-
+	// updates up to three fields: phone, isArtist, soundCloud, all other fields passed are ignored
+	// if update is successful, returns the updated user model
 	app.put('/user/:id', async (req, res) => {
-		const unsername = req.params.id;
-		const { phone, isArtist, joinDate, soundCloud } = req.body;
+		const username = req.params.id;
+		const { phone, isArtist, soundCloud } = req.body;
 		let options = {};
 		console.log(phone);
 		if (phone) {
@@ -77,16 +78,11 @@ module.exports = function(app, db) {
 		if (isArtist) {
 			options.isArtist = isArtist;
 		}
-		if (joinDate) {
-			options.joinDate = joinDate;
-		}
 		if (soundCloud) {
 			options.soundCloud = soundCloud;
 		}
 		try {
-			const updatedUser = await User.findOneAndUpdate(username, options, { runValidators: true });
-			console.log('test');
-			console.log(await updatedUser);
+			const updatedUser = await User.findOneAndUpdate({ username: username}, { $set: options }, { runValidators: true, context: 'query' });
 			if (!updatedUser) {
 				res.status(404).send('User not found error: ' + username);
 			} else {
@@ -94,9 +90,9 @@ module.exports = function(app, db) {
 			}
 		} catch (err) {
 			if (err.name === 'MongoError') {
-				res.status(409).send('There was an issue saving to db: ', [err.message]);
+				res.status(409).send('There was an issue saving to db: ' + err.message);
 			}
-			res.status(500).send('Unknown Server Error: ', [err.message]);
+			res.status(500).send('Unknown Server Error: ' + err.message);
 		}
 	})
 
